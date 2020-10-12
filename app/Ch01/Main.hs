b768f9301e6420731de110c6d7bde6b0d6522d62
Eduardo Ramón Lestau <erlestau@gmail.com>
2020-10-13 01:28:59 +0200

    Example WC and move app/Main.hs to app/sample/Main.hs

---
+++ app/Ch01/Main.hs
@@ -0,0 +1,27 @@
+-- |
+-- Module      :  Ch01.WC
+-- Description :  like `wc` utility 
+-- Copyright   :  erlnow 2020 - 2030
+-- License     :  BSD3
+--
+-- Maintainer  :  erlestau@gmail.com
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+-- A simple program. Example from Chapter 1 __Getting Started__, p. 15
+-- 
+-- Original source:
+--
+-- @
+-- file: ch01/WC.hs
+-- lines beginning wit "--" are comments.
+--
+-- main = interat wordCount
+--     where wordCount input = show (length (lines input) ++ "\n"
+-- @
+
+module Main (main) where
+
+main :: IO()
+main = interact wordCount
+  where wordCount input = show (length (lines input)) ++ "\n"
